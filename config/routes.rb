16483d619ecf5a6880a57ea5cf1e513bf62a7e4f
Mateusz Boczula <kareem.jabaar@gmail.com>
2012-03-02 22:54:33 +0100

    Uklad templatow ustawiony, polaczenie z PostgreSQL

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Cursive::Application.routes.draw do
+  get "site/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -49,6 +51,7 @@ Cursive::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
+  root to: 'site#index', as: 'site_index'
 
   # See how all your routes lay out with "rake routes"
 
