1f93f4cffb8040ab0c33f0a4ec4a65da3247280e
Mateusz Boczula <kareem.jabaar@gmail.com>
2012-03-02 18:30:46 +0100

    Wygenerowano szkielet projektu

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cursive::Application.config.secret_token = '18d0ce62e305c2c4b1dcbdd491780fd1364a8904349cab44b23c1b7f7b98393857dfaf9e57208d4f236972e54c03157faf1330ba625ecf365e7357fdfd03607b'
